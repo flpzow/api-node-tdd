d13c1a87b8864d1c52ddaf11efb8844eadf70fad
Felipe de Castro <castrofe55@gmail.com>
2022-06-24 16:13:21 -0300

    ensure LoginController returns 400 is no password is provided

--- src/presentation/controllers/login-controller.js
+++ src/presentation/controllers/login-controller.js
@@ -1,6 +1,6 @@
 module.exports = class LoginController {
   handle(httpRequest) {
-    if(!httpRequest.body.email) {
+    if(!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
--- src/presentation/controllers/login-controller.spec.js
+++ src/presentation/controllers/login-controller.spec.js
@@ -13,4 +13,17 @@ describe('Login Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
   })
+
+  test('should return 400 if no password is provided', () => {
+    const sut = new LoginController();
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
 })
